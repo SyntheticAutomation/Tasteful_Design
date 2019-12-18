520c29df32cfea6a08e5861e176ccce664436042
SyntheticAutomation <syntheticauto900@gmail.com>
2019-12-18 11:10:35 -0700

    Adds carousel of images from api response

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,24 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// $(document).ready(function() {
+//   $(".btn-discovery").click(function() {
+//     alert("This button doesn't do shit yet!");
+//     // when the button is clicked we want to load all the images. 
+
+
+
+
+
+//     // when the image is loaded, check if the image link already exists in our DB
+//     // if it doesn't, save it to the database
+//     // if it does, do nothing
+//     //  ^ should this be done in the controller? orrrr a model? or both? probably not here.
+//   });
+// });
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-    
+    if params[:commit]
+      @search_word = params[:query]
+      key = ENV['unsplash_access_key']
+      response = UnsplashService.search(@search_word, key)
+      @photos = response.body[:results]
+    end
   end
 end
